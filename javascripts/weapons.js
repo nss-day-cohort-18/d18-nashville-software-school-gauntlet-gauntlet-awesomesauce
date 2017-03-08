2d30b736ac9fbf4ce8b0631369169dec045b9348
Helana <helanan@gmail.com>
2017-03-08 13:27:28 -0600

    debug weapon inheritance functionality

--- javascripts/weapons.js
+++ javascripts/weapons.js
@@ -19,33 +19,34 @@ OldGauntlet.WeaponsContainer.Dagger = function() {
 };
 OldGauntlet.WeaponsContainer.Dagger.prototype = new OldGauntlet.WeaponsContainer.Weapon();
 
-OldGauntlet.WeaponsContainer.BroadSword.prototype = function() {
+OldGauntlet.WeaponsContainer.BroadSword = function() {
   this.name = "Broad Sword";
   this.damage = 14;
   this.hands = 2;
 };
 OldGauntlet.WeaponsContainer.BroadSword.prototype = new OldGauntlet.WeaponsContainer.Weapon();
 
-OldGauntlet.WeaponsContainer.WarAxe.prototype = function() {
+OldGauntlet.WeaponsContainer.WarAxe = function() {
   this.name = "War Axe";
   this.damage = 18;
   this.hands = 2;
 };
-OldGauntlet.WeaponsCase.WarAxe.prototype = new OldGauntlet.WeaponsContainer.Weapon();
+OldGauntlet.WeaponsContainer.WarAxe.prototype = new OldGauntlet.WeaponsContainer.Weapon();
 
-OldGauntlet.WeaponsContainer.Bubbles.prototype = function() {
+OldGauntlet.WeaponsContainer.Bubbles = function() {
   this.name = "Bubbles";
   this.damage = 1;
   this.hands = 1;
 };
-OldGauntlet.WeaponsCase.Bubbles.prototype = new OldGauntlet.WeaponsContainer.Weapon();
+OldGauntlet.WeaponsContainer.Bubbles.prototype = new OldGauntlet.WeaponsContainer.Weapon();
 
-OldGauntlet.WeaponsContainer.Spear.prototype = new OldGauntlet.WeaponsContainer.Spear();
+OldGauntlet.WeaponsContainer.Spear = function() {
   this.name = "Spear";
   this.damage = 15;
   this.hands = 2;
 };
-OldGauntlet.WeaponsCase.Spear.prototype = new OldGauntlet.WeaponsContainer.Weapon();
+
+OldGauntlet.WeaponsContainer.Spear.prototype = new OldGauntlet.WeaponsContainer.Weapon();
 
 
 return OldGauntlet;
